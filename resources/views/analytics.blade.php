4c7c0524f06866f04413c84dbf45b1c7531c5106
AlbertWebs <albertmuhatia@gmail.com>
2024-10-07 12:21:07 +0300

    update

--- resources/views/analytics.blade.php
+++ resources/views/analytics.blade.php
@@ -1,10 +1,11 @@
-   <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-E420ZWQVBN"></script>
-        <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
 
-        gtag('config', 'G-E420ZWQVBN');
-        </script>
-    <!-- Scripts -->
+   <!-- Google tag (gtag.js) -->
+   <script async src="https://www.googletagmanager.com/gtag/js?id=G-E420ZWQVBN"></script>
+   <script>
+   window.dataLayer = window.dataLayer || [];
+   function gtag(){dataLayer.push(arguments);}
+   gtag('js', new Date());
+
+   gtag('config', 'G-E420ZWQVBN');
+   </script>
+<!-- Scripts -->
